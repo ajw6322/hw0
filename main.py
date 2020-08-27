620d1156abeea032bbaac8c856576c5e43a7a4bc
ajw6322 <ajw6322@psu.edu>
2020-08-27 21:37:46 +0000

    spelling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 c = input("Enter temperature in celsius: ")
 c = float(c)
 f = (c*(9/5))+32
-print(str(c) + '\u00b0' + " in celsius is equivalent to " + str(f) + '\u00b0' + " Fahrenheit.")
+print(str(c) + '\u00b0' + " in Celsius is equivalent to " + str(f) + '\u00b0' + " Fahrenheit.")
